f816d3bd2f0d637e85ca0e4390122498d7607f60
Your Name <deckerwill7@gmail.com>
2024-01-08 10:04:26 -0600

    update

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 ######## Plotting via brain_plotting.py for reproducibility ########
 
 # import module
-from brain_plotting import surface_plot
+from brain_plotting import surface_plot, plot_hoc_label
 
-surface_plot(roi=34, roi_name='stg', hemi='left', brain_shape=1, colormap='black_purple_r', dir='/Users/lendlab/Desktop/', file_extension='.png')
+# plot ROI on cortex
+surface_plot(roi=34, roi_name='stg', hemi='left', brain_shape=1, 
+             colormap='black_purple_r', dir='/Users/lendlab/Desktop/', file_extension='.png')
+
+# plot ROI on 2d plane
+plot_hoc_label(roi=15, figtitle='Test', axis='x', depth=None)
