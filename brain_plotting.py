1b36264a68aee0c3feeca6016852869f04755b35
Your Name <deckerwill7@gmail.com>
2024-01-08 10:04:21 -0600

    add `plot_hoc_label()`

--- brain_plotting.py
+++ brain_plotting.py
@@ -1,5 +1,101 @@
 #!/usr/bin env python
 
+from nilearn import datasets, plotting
+import nibabel as nib
+import os
+import matplotlib.pyplot as plt 
+import numpy as np
+
+def plot_hoc_label(roi: int, figtitle: str, axis: str or int, depth: int):
+    """Plot label from Harvard Oxford Cortical-cortl-maxprob-thr50-1mm Atlas on 2d plane
+    
+    Parameters
+    ----------
+    atlas: nii file
+        nibabel compatible Nifti file from Nilearn
+
+    figtitle: str
+        Title of resultant figure
+
+    axis: str or int
+        X(0), Y(1), or Z(2)
+
+    depth: int
+        How much
+
+    Returns
+    -------
+    X: ndarray
+        numpy ndarray for matplotlib.pyplot.imshow()
+    """
+    # parse some inputs
+    if axis is str:
+        if axis.lower() == 'x':
+            axis = 0
+        elif axis.lower() == 'y':
+            axis = 1
+        elif axis.lower() == 'z':
+            axis = 2
+
+    if depth is None:
+        depth = 4
+    
+    # get atlas
+    atlas = datasets.fetch_atlas_harvard_oxford("cortl-maxprob-thr50-1mm")
+
+    # convert to array
+    img = np.squeeze(nib.load(atlas["filename"]).get_fdata())
+
+    # get some important img points
+    img_size = img.shape
+    extent = (img_size[0], 0,img_size[0], 0)
+    d = img.shape[0]
+    extent = (d, 0, d, 0)
+    f = int(img_size[0]/depth)
+
+    if axis is 0:
+        parcel = img[f, :, :] == roi
+        
+        if roi not in img:
+            raise ValueError(f'Incorrect ROI selection.\n Please select from the following:\n\
+                             {np.unique(atlas["maps"].get_fdata())}')
+        
+        for i in range(f):
+            plt.imshow(img[i, :, :], cmap='binary',  extent=extent)
+
+        plt.imshow(parcel, alpha=0.8, cmap='viridis', extent=extent)
+
+    elif axis is 1:
+        parcel = img[:, f, :] == roi
+    
+        if roi not in img:
+            raise ValueError(f'Incorrect ROI selection.\n Please select from the following:\n\
+                                {np.unique(atlas["maps"].get_fdata())}')
+        
+        for i in range(f):
+            plt.imshow(img[:, i, :], cmap='binary',  extent=extent)
+
+        plt.imshow(parcel, alpha=0.8, cmap='viridis', extent=extent)
+
+    elif axis is 2:
+        parcel = img[:, :, f] == roi
+    
+        if roi not in img:
+            raise ValueError(f'Incorrect ROI selection.\n Please select from the following:\n\
+                                {np.unique(atlas["maps"].get_fdata())}')
+        
+        for i in range(f):
+            plt.imshow(img[:, :, i], cmap='binary',  extent=extent)
+
+        plt.imshow(parcel, alpha=0.8, cmap='viridis', extent=extent)
+
+
+
+    plt.axis('off')
+    plt.title(figtitle)
+
+    return(parcel)
+
 def surface_plot(roi, roi_name, hemi, brain_shape, colormap, dir, file_extension):
         """
         Plotting coritcal ROIs using Destrieux atlas for demonstration purposes.
@@ -43,9 +139,6 @@ def surface_plot(roi, roi_name, hemi, brain_shape, colormap, dir, file_extension
                 file_extension='.png')
 
         """
-
-        from nilearn import datasets, plotting
-        import os
 
         # parse input arguments
         roi = roi
